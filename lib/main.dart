ddb9a037c89d8a08283cbac4f526a336c7c7e157
Waqar Ahmed <57212390+WaqarAhmedk@users.noreply.github.com>
2021-05-04 20:52:29 +0500

    learned how to use row column continaer listtile card widget
    in it i created a micard bussiness card which shows some of myu information having fun

--- lib/main.dart
+++ lib/main.dart
@@ -7,29 +7,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(),
-                Text("Waqar Ahmed"),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(icon: Icon(Icons.call), onPressed: () {}),
-                    Text("342432843"),
-                  ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("images/myimage.jpg"),
+              ),
+              Text(
+                "Waqar Ahmed",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Pacifico",
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "FLUTTER DEVOLPER",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontFamily: "SourceSansPro-Regular"),
+              ),
+              SizedBox(
+                height: 13,
+                width: 150,
+                child: Divider(
+                  color: Colors.white,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(icon: Icon(Icons.message), onPressed: () {}),
-                    Text("Wlwaqarah0@gmail.com"),
-                  ],
+              ),
+              Card(
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                child: ListTile(
+                  leading: Icon(Icons.phone),
+                  title: Text("0346-1599447"),
                 ),
-              ],
-            ),
+              ),
+              Card(
+                margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                child: ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text("wlwaqarah0@gmail.com"),
+                ),
+              ),
+            ],
           ),
         ),
       ),
